c9f1a5fac895bc6855c4a9a9496b6461cd69facc
Andrew Micheal <mykmicheals@gmail.com>
2023-05-26 21:55:26 +0100

    added subject upload url

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,12 @@ from .views import *
 
 urlpatterns = [
     path('user/', get_user_details),
-    path('students/',StudentListView.as_view()),
+    path('students/', StudentListView.as_view()),
     path('teachers/', TeacherListView.as_view()),
     path('subjects/', SubjectListCreateAPIView.as_view()),
     path('mysubjects/', TeacherSubjectsListAPIView.as_view()),
+    path('mysubjects/<int:subject_id>/', SubjectStudentsListAPIView.as_view()),
+    path('mysubjectscore/<int:subject_id>/', StudentScoreViews.as_view()),
+
 
 ]
