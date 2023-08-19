e8658038d80c9ac4fdf9e6a0d0889748a32bc7c9
amMicheals <mandrew@insight7.io>
2023-08-19 04:51:53 +0100

    writing test casses

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+
+
+class MyApiTestCase(TestCase):
+    def setup(self):
+        self.client=APIClient
+
+    def myApiView(self):
+        response = self.client.get('/myapi')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'message': 'Success'})
 
 # Create your tests here.
