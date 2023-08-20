d914429c161bb4b3b3b83bd6bc25f82a6fa2c938
amMicheals <mandrew@insight7.io>
2023-08-20 05:27:18 +0100

    Allowed cors origin

--- schoolBackend/settings.py
+++ schoolBackend/settings.py
@@ -138,6 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    "https://stpaulsagam.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
